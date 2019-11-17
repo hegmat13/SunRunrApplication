95dcbd17662c7510e8e835c28eabf72941e421c5
Philippe Cutillas <pcutillas@email.arizona.edu>
2019-11-17 14:07:04 -0700

    updted registration and user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ var db = require("../db");
 var userSchema = new db.Schema({
   email:        { type: String, required: true, unique: true },
   passwordHash: String,
+  zipcode:      Number,
   lastAccess:   { type: Date, default: Date.now },  // TODO: Replace here down with correct information
-  userDevices:  [ String ],
-  potholesHit:  [ { potholeId: Number, numHits: Number } ]
+  userDevices:  [ String ]
 });
 
 var User = db.model("User", userSchema);
