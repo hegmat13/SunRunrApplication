cf5228355dffecbd5ee5d05e185d30d7913a8f3c
hegmat13 <heger@email.arizona.edu>
2019-11-19 15:45:47 -0700

    modified schema

--- models/hwdata.js
+++ models/hwdata.js
@@ -4,11 +4,11 @@ var db = require("../db");
 var hwDataSchema = new db.Schema({
     apikey:     Number,
     deviceId:   String,
-    //userEmail:  String,
-    latitude:   Number,
-    longitude:  Number,
-    uv:         Number
-    //submitTime: { type: Date, default: Date.now }
+    GPS_speed:  String,
+    lat:        Number,
+    lon:        Number,
+    uv:         Number,
+    publishTime: { type: Date, default: Date.now }
 });
 
 // Creates a Devices (plural) collection in the db using the device schema
