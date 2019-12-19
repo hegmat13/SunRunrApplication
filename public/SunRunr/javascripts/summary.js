9d744e433f8f823cd34fdf5d7d876b6abe8cea60
Philippe <pcutillas@email.arizona.edu>
2019-12-18 22:59:01 -0700

    added calculations and activity making to summary

--- public/SunRunr/javascripts/summary.js
+++ public/SunRunr/javascripts/summary.js
@@ -86,52 +86,115 @@ function loadDeviceData() {
     dataType: 'json'
   })
     .done(loadSuccess)
-    .fail(loadSuccess); // TODO: Change to loadError after testing
+    .fail(loadError);
 }
 
 function loadSuccess(data, textStatus, jqXHR) { // TODO: Needs to change
   
   console.log(data); 
-  if (true) { //TODO: data.success
+  if (data.success) {
     let addHTML = '<p>Recorded posts from device:</p>';
 
     $('.formToRemove').addClass('hide');
-    let GPS_speed;
-    let lat;
-    let lon;
-    let uv = 0.000000;
-    let publishTime; 
     let uvtotal = 0.000000; 
     let totalduration = 0.000000;
     let totalCal = 0.000000; 
     let activityList = [];
 
     for (let list of data.data) { //each list is one device ID's list of hwData
-      
-      totalduration += (list[list.length - 1] - list[0])/60000; //time in minutes of activity
+    let activity = [];
+      let totDist = 0;
+      let totSpeed = 0;
+      let uvtot = 0;
+      for (let i in list) { // getting all hits of a device
+        let hit = list[i];
+        let prevHit = list[i-1];
+        if (i != 0) {
+          if (hit.publishTime > (prevHit.publishTime + 900000)) { //15 minutes seperates 2 activities
+            let date = Date(activity[0].publishTime).toString().substr(4, 11);
+            
+            let avgSpeed = totSpeed/activity.length;
+            if (avgSpeed <= 3.1) { //avg walking speed
+              let type = "Walking";
+              let calories = 80 * totDist; //80 cal per mile walking
+            }
+            else if (avgSpeed <= 8.3) { //avg running speed
+              let type = "Running";
+              let calories = 100 * totDist; //100 cal per mile
+            }
+            else { //biking
+              let type = "Biking";
+              let calories = 48 * totDist; //48 cal per mile
+            }
+            let duration = (activity[activity.length - 1] - activity[0])/60000; //time in minutes of activity
+            uvAvg = uvtot/activity.length;
+
+            activityList.push({data: activity, date: date, dist: totDist, time: activity[0].publishTime, uv: uvAvg, speed: avgSpeed, type: type, calories: calories, duration: duration}); //sorts activities by most recent
+            
+            activity = [];
+
+            totDist = 0;
+            totSpeed = 0;
+            uvtot = 0;
+
+            totSpeed += hit.GPS_Speed * 1.15078; // to mph
+            activity.push(hit);
+            uvtot += parseInt(hit.uv);
+            continue;
+          }
+        }
+        else {
+          totSpeed += hit.GPS_Speed * 1.15078; // to mph
+          activity.push(hit);
+          uvtot += parseInt(hit.uv);
+          continue;
+        }
+        totDist += distance(prevHit.lat, prevHit.lon, hit.lat, hit.lon, 'M'); //to miles
+        totSpeed += hit.GPS_Speed * 1.15078;
+        uvtot += parseInt(hit.uv);
+      }
+    }
+    
+    let i = 0;
+    for (let activity of activityList) {
+      if ((Date.now() - activity.time) < 604800000) { //activities from within a week from current date
+        totalduration += activity.dist;
+        totalCal += activity.calories;
+        uvtotal += activity.uv;
+        i += 1;
+      }
     }
 
-
-    for (let obj of data.data) {
-      GPS_speed = obj.GPS_speed;
-      lat = obj.lat;
-      lon = obj.lon;
-      uv = obj.uv;
-      publishTime = obj.publishTime;
-      uvtotal += parseInt(uv); 
-     // addHTML += '<ul><li>Published: ' + publishTime + '</li><li>Latitude/Longitude: ' + lat + ' / ' + lon + '</li><li>Speed: ' + GPS_speed + ' knots</li><li>UV Reading: ' + uv + ' mW/cm&#178;</li></ul><br>';
-    }
-
-    console.log(uvtotal); 
-
-    $("#uv").html(uvtotal); 
-    $("#duration").html(publishTime); 
-
-    // parToEdit.html(addHTML);    
+    $("#uv").html(uvtotal/i); 
+    $("#duration").html(totalduration); 
+    $('#calories').html(totalCal);
+ 
   }
   else {
     console.log(data.errors)
   }
+}
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
 }
   
 function loadError(jqXHR, textStatus, errorThrown) { // TODO: Needs to change
